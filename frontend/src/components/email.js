4d78017fc9b0e48fcb64c63840a36542499b9ddb
biancadevelop <[bianca.manage@gmail.com]>
2020-10-27 18:37:45 -0700

    started creating email capture component, need to integrate and style klayvio

---
+++ frontend/src/components/email.js
@@ -0,0 +1,31 @@
+/**
+ * Email Capture Component with Klayvio Email Marketing Integration
+ */
+import React from "react"
+import "../email.css"
+
+const Email = () => {
+
+    return(
+        <div className="email-section">
+            <div className="inner-email-container">
+                <div className="email-text">
+                    <div className="email-main-text">Join the Boomer Family</div>
+                    <div className="email-sub-text">Sign up and get <b>10% OFF</b> on your first order!</div>  
+                </div>
+                    <div class="klaviyo-form-TxM3pR"></div> 
+            </div>
+
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+export default Email
