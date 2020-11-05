d86aa2f397f5d7c3fac54e413b71b1c408f0a332
Brian Vargas <cbvargas123@gmail.com>
2020-11-05 11:12:17 -0800

    rendering list of products from query data

--- frontend/src/components/product-carousel.js
+++ frontend/src/components/product-carousel.js
@@ -2,28 +2,110 @@ import React, { useState, useEffect } from "react"
 // import PropTypes from "prop-types"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import "../styles/product-carousel.css"
+import Product from "./product"
 
 
 const ProductCarousel = () => {
+
     const [clickedNavBtn, setClickedNavBtn] = useState("New Arrivals")
     const [renderedProductsArray, setRenderedProductsArray] = useState([])
 
-    // perform query when clickedNavBtn is updated
+    const data = useStaticQuery(graphql`
+    {
+      allShopifyCollection(
+        filter: {title: {regex: "/New Arrivals|Most Popular|On Sale Items/"}},
+        sort: {fields: title}
+      ) {
+        nodes {
+          shopifyId
+          title
+          descriptionHtml
+          image {
+            altText
+            src
+          }
+          internal {
+            content
+            description
+            ignoreType
+            mediaType
+          }
+          products {
+            title
+            onlineStoreUrl
+            descriptionHtml
+            availableForSale
+            totalInventory
+            images {
+              altText
+              originalSrc
+            }
+            priceRange {
+              maxVariantPrice {
+                amount
+                currencyCode
+              }
+              minVariantPrice {
+                currencyCode
+                amount
+              }
+            }
+            productType
+            tags
+            variants {
+              title
+              selectedOptions {
+                name
+                value
+              }
+              priceV2 {
+                amount
+                currencyCode
+              }
+              image {
+                altText
+                originalSrc
+              }
+              availableForSale
+            }
+          }
+        }
+      }
+    }
+  `)
+
+    // Array of shopify nodes is in alphabetical order.  Make sure to destructure array in same order 
+    const [MOST_POPULAR_DATA, NEW_ARRIVALS_DATA, ON_SALE_DATA] = data.allShopifyCollection.nodes
+
+    const NAV_TITLE_ARR = [NEW_ARRIVALS_DATA.title, MOST_POPULAR_DATA.title, ON_SALE_DATA.title]
+
+    // Products array is updated with array from query data that matches clicked button
     useEffect(() => {
-        console.log('Current Products: ', renderedProductsArray, clickedNavBtn)
+        const [dataFilteredOnClick] = data.allShopifyCollection.nodes.filter(({ title }) => title == clickedNavBtn)
+        setRenderedProductsArray(dataFilteredOnClick.products)
+        console.log('clicked data:', dataFilteredOnClick.products)
     }, [clickedNavBtn])
 
     const handleNavClick = (e) => {
-        // title constant is the title of the clicked nav btn
+        // value constant is the title of the clicked nav btn
         const { value } = e.target
         setClickedNavBtn(value)
     }
 
-    const NAV_TITLE_ARR = ["New Arrivals", "Most Popular", "On-Sale"]
-
     const NAV_LIST_ITEMS = NAV_TITLE_ARR.map((title, index) => (
         <li key={index}>
             <button className="carousel-nav-btn" onClick={handleNavClick} value={title}>{title}</button>
+        </li>
+    ))
+
+    const PRODUCTS_LIST_ITEMS = renderedProductsArray.map((productObject, index) => (
+        <li key={index}>
+            <Product
+                title={productObject.title}
+                minPrice={productObject.priceRange.minVariantPrice.amount}
+                mainImageSrc={productObject.images[0].originalSrc}
+                imageAltText={productObject.images[0].altText}
+            />
         </li>
     ))
 
@@ -33,9 +115,14 @@ const ProductCarousel = () => {
                 <ul className="carousel-nav-wrapper">
                     {NAV_LIST_ITEMS}
                 </ul>
-                <div className="redirect-btn">
-                    <Link to="/">View All</Link>
-                </div>
+                <Link to="/">
+                    <div className="redirect-btn">View All</div>
+                </Link>
+            </div>
+            <div className="product-carousel-container">
+                <ul className="product-carousel-wrapper">
+                    {PRODUCTS_LIST_ITEMS}
+                </ul>
             </div>
         </div>
     )
