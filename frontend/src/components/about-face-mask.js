b1d93369744432e453c4c8e91877790a0aa7328f
Brian Vargas <cbvargas123@gmail.com>
2020-11-09 10:33:15 -0800

    completed core jsx for component

--- frontend/src/components/about-face-mask.js
+++ frontend/src/components/about-face-mask.js
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import "../styles/about-face-mask.css"
+import MainButton from "./main-button"
 
+import MastImg from "../../content/assets/mask.png"
 
 const AboutFaceMask = () => {
     return (
-        <div>
-            Hello From Face Mask Component
-        </div>
+        <section className="about-face-mask-section">
+            <h4>About Our Face Mask</h4>
+            <div className="image-container">
+                <img src={MastImg} alt="Boomer Naturals Mask Information"/>
+                <span className="pulse-btn"></span>
+                <span className="pulse-btn"></span>
+                <span className="pulse-btn"></span>
+                <span className="pulse-btn"></span>
+            </div>
+            <div className="extra-info-container">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus corporis, ratione blanditiis commodi eum repudiandae dolore deleniti laborum.</p>
+                <div className="redirect-btns">
+                    <MainButton text="Learn More"/>
+                    <Link to="/">Customize Mask</Link>
+                </div>
+            </div>
+        </section>
     )
 }
 
