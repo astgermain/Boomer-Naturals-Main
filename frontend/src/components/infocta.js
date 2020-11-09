3a0a86d264ac08e3a9af6c46ee33d09b4285f196
Andrew St Germain <38926995+astgermain@users.noreply.github.com>
2020-11-09 09:23:07 -0800

    Removed import

--- frontend/src/components/infocta.js
+++ frontend/src/components/infocta.js
@@ -4,7 +4,6 @@
 import React, { useState, useEffect } from "react"
 import propTypes from 'prop-types'
 import { Link } from "gatsby"
-import Slider from "./slider"
 import "../infocta.css"
 
 const Infocta = ({svgProp, maintextProp, subtextProp, buttontextProp}) => {
