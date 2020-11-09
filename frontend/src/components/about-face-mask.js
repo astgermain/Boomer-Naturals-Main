fd0b9b47077ef07a43ba9fccd3c362257ed2855f
Brian Vargas <cbvargas123@gmail.com>
2020-11-09 12:47:30 -0800

    created hover event tracking state and methods

--- frontend/src/components/about-face-mask.js
+++ frontend/src/components/about-face-mask.js
@@ -6,20 +6,73 @@ import MainButton from "./main-button"
 import MastImg from "../../content/assets/mask.png"
 
 const AboutFaceMask = () => {
+
+    const [btnState, setBtnState] = useState({
+        btnOne: "hide",
+        btnTwo: "hide",
+        btnThree: "hide",
+        btnFour: "hide",
+    })
+
+    const handleHoverEvent = hoveredBtn => {
+
+        const changeBtnState = btn =>
+            btnState[btn] == "hide"
+                ?
+                setBtnState({ ...btnState, [btn]: "show" })
+                :
+                setBtnState({ ...btnState, [btn]: "hide" })
+
+        switch (hoveredBtn) {
+            case 1:
+                changeBtnState("btnOne")
+                break;
+            case 2:
+                changeBtnState("btnTwo")
+                break;
+            case 3:
+                changeBtnState("btnThree")
+                break;
+            case 4:
+                changeBtnState("btnFour")
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <section className="about-face-mask-section">
             <h4>About Our Face Mask</h4>
             <div className="image-container">
-                <img src={MastImg} alt="Boomer Naturals Mask Information" width="100%" height="auto"/>
-                <span className="pulse-btn pulse-btn-1"></span>
-                <span className="pulse-btn pulse-btn-2"></span>
-                <span className="pulse-btn pulse-btn-3"></span>
-                <span className="pulse-btn pulse-btn-4"></span>
+                <img src={MastImg} alt="Boomer Naturals Mask Information" width="100%" height="auto" />
+                <span
+                    onMouseEnter={() => handleHoverEvent(1)}
+                    onMouseLeave={() => handleHoverEvent(1)}
+                    className="pulse-btn pulse-btn-1"
+                >
+                    <div className={`info-modal ${btnState.btnOne}`}>This will say something</div>
+                </span>
+                <span
+                    className="pulse-btn pulse-btn-2"
+                    onMouseEnter={() => handleHoverEvent(2)}
+                    onMouseLeave={() => handleHoverEvent(2)}
+                ></span>
+                <span
+                    className="pulse-btn pulse-btn-3"
+                    onMouseEnter={() => handleHoverEvent(3)}
+                    onMouseLeave={() => handleHoverEvent(3)}
+                ></span>
+                <span
+                    className="pulse-btn pulse-btn-4"
+                    onMouseEnter={() => handleHoverEvent(4)}
+                    onMouseLeave={() => handleHoverEvent(4)}
+                ></span>
             </div>
             <div className="extra-info-container">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus corporis</p>
                 <div className="redirect-btns">
-                    <MainButton text="Learn More"/>
+                    <MainButton text="Learn More" />
                     <Link to="/">Customize Mask</Link>
                 </div>
             </div>
