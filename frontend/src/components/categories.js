21f3714385c3e96dac68e68a06d879b8109f54f8
biancadevelop <[bianca.manage@gmail.com]>
2020-11-13 13:07:25 -0800

    changes to categories need to fix

--- frontend/src/components/categories.js
+++ frontend/src/components/categories.js
@@ -1,7 +1,7 @@
 /**
  * Category Component
- *  
- * 
+ *
+ *
  */
 
 import React from "react"
@@ -9,41 +9,51 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import "../styles/categories.css"
 
-
 const Categories = () => {
- 
+    
   return (
     <section className="category-container">
-        {/* Facemask category button */}
-        <Link to="/" className="full-category cat1">
-            <span className="category-text">Face Mask</span>   
-        </Link>
-        {/* Gaiters category button */}
-        <Link to="/" className="full-category cat2">
-            <span className="category-text">Gaiters</span>
-        </Link>
-        {/* PPE category button */}
-        <Link to="/" className="full-category cat3">
-            <span className="category-text">PPE</span>
-        </Link>
-        <div className="category-group">
-            {/* Botanics category button */}
-            <Link to="/" className="half-category">
-                <span className="category-text-half">Botanics</span>
-            </Link>
-            {/* Skin Care category button */}
-            <Link to="/" className="half-category">
-                <span className="category-text-half">Skin Care</span>
-            </Link>
+      {/* Facemask category button */}
+      <Link to="/" className="full-category cat1">
+        <div className="viewMoretext">
+          View More
         </div>
+        <span className="category-text">Face Mask</span>
+      </Link>
+
+      {/* Gaiters category button */}
+      <Link to="/" className="full-category cat2">
+        <span className="category-text">Gaiters</span>
+      </Link>
+      {/* PPE category button */}
+      <Link to="/" className="full-category cat3">
+        <div className="viewMoretext">
+          View More
+        </div>
+        <span className="category-text">PPE</span>
+      </Link>
+      <div className="category-group">
+        {/* Botanics category button */}
+        <Link to="/" className="half-category">
+        <div className="viewMoretext">
+          View More
+        </div>
+          <span className="category-text-half">Botanics</span>
+        </Link>
+        {/* Skin Care category button */}
+        <Link to="/" className="half-category">
+        <div className="viewMoretext">
+          View More
+        </div>
+          <span className="category-text-half">Skin Care</span>
+        </Link>
+      </div>
     </section>
   )
 }
 
-Categories.defaultProps = {
-}
+Categories.defaultProps = {}
 
-Categories.propTypes = {
-}
+Categories.propTypes = {}
 
 export default Categories
