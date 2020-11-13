70dee8f2084a6f4db1dd3bc256718d47b4a7d81f
Brian Vargas <cbvargas123@gmail.com>
2020-11-12 16:12:55 -0800

    completed logic for displaying result info

--- frontend/src/components/search-results.js
+++ frontend/src/components/search-results.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react"
 import Product from "./product";
+import { Link } from "gatsby"
 import "../styles/search.css"
 
 const SearchResults = ({ productsArray, searchInput }) => {
     const [modalShow, setModalShow] = useState("")
+
+    // Num of products to display
+    const ITEMS_TO_SHOW = 10
 
     const handleModalShow = e => {
         setModalShow(e)
     }
     console.log("products from props:", productsArray)
 
-    const PRODUCT_LIST_ITEMS = productsArray.map(
+    // Creates array of product components with length of ITEMS_TO_SHOW amount
+    const PRODUCT_LIST_ITEMS = productsArray.slice(0, ITEMS_TO_SHOW).map(
         product =>
             <li key={product.shopifyId}>
                 <Product productInfo={product} handleModalShow={handleModalShow} />
@@ -21,14 +26,21 @@ const SearchResults = ({ productsArray, searchInput }) => {
         <li className="no-results">Your search for "{searchInput}" did not yield any results.</li>
     )
     
+    // creates "results-info" text with products being presented and length of results as input
+    const createResultsInfo = (numShowing, arrLength) => {
+        const showingTxt = `${numShowing < arrLength ? numShowing : arrLength} of ${arrLength}`
+        return `Showing ${arrLength ? showingTxt : 0} result${arrLength === 1 ? "" : "s"} for "${searchInput}"`
+    }
+
     return (
         <div className="search-results">
             <span className="results-info">
-                Showing {productsArray.length} result{productsArray.length === 1 ? "" : "s"} for "{searchInput}"
+                {createResultsInfo(ITEMS_TO_SHOW, productsArray.length)}
             </span>
             <ul className="results-list">
                 {productsArray.length ? PRODUCT_LIST_ITEMS : NO_RESULTS_MSG}
             </ul>
+
         </div>
     )
 }
