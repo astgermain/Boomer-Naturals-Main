9e1f50562c9b3983b2c8f759ac11620ed044b829
biancadevelop <[bianca.manage@gmail.com]>
2020-11-16 16:51:41 -0800

    fixed the categories

--- frontend/src/components/categories.js
+++ frontend/src/components/categories.js
@@ -10,42 +10,45 @@ import { Link } from "gatsby"
 import "../styles/categories.css"
 
 const Categories = () => {
-    
   return (
     <section className="category-container">
       {/* Facemask category button */}
       <Link to="/" className="full-category cat1">
-        <div className="viewMoretext">
-          View More
-        </div>
-        <span className="category-text">Face Mask</span>
+       <span className="category-text">Face Mask</span>
+       <div className="overlay">
+         <div className="viewMoreText">View More</div>  
+       </div>
+        
       </Link>
 
       {/* Gaiters category button */}
       <Link to="/" className="full-category cat2">
         <span className="category-text">Gaiters</span>
+        <div className="overlay">
+         <div className="viewMoreText">View More</div>  
+       </div>
       </Link>
       {/* PPE category button */}
       <Link to="/" className="full-category cat3">
-        <div className="viewMoretext">
-          View More
-        </div>
         <span className="category-text">PPE</span>
+        <div className="overlay">
+         <div className="viewMoreText">View More</div>  
+       </div>
       </Link>
       <div className="category-group">
         {/* Botanics category button */}
         <Link to="/" className="half-category">
-        <div className="viewMoretext">
-          View More
-        </div>
           <span className="category-text-half">Botanics</span>
+          <div className="overlay">
+         <div className="viewMoreText">View More</div>  
+       </div>  
         </Link>
         {/* Skin Care category button */}
         <Link to="/" className="half-category">
-        <div className="viewMoretext">
-          View More
-        </div>
           <span className="category-text-half">Skin Care</span>
+          <div className="overlay">
+         <div className="viewMoreText">View More</div>  
+       </div>
         </Link>
       </div>
     </section>
