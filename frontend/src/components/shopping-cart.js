0671d480de4578ef8c1215cb21de7ff9a436f4c5
Brian Vargas <cbvargas123@gmail.com>
2020-12-01 14:23:56 -0800

    London Bridge is falling down

--- frontend/src/components/shopping-cart.js
+++ frontend/src/components/shopping-cart.js
@@ -4,14 +4,21 @@ import Client from 'shopify-buy'
 const { GATSBY_STOREFRONT_TOKEN } = process.env
 
 const ShoppingCart = () => {
-    const client = Client.buildClient({
-        domain: 'boomerfacemasks.myshopify.com',
-        storefrontAccessToken: GATSBY_STOREFRONT_TOKEN
-    })
-    client.checkout.create()
-        .then((checkout) => {
-            console.log('checkout from client:',checkout)
+
+    const [checkout, setCheckout] = useState({})
+
+    useEffect(() => {
+        const client = Client.buildClient({
+            domain: 'boomerfacemasks.myshopify.com',
+            storefrontAccessToken: GATSBY_STOREFRONT_TOKEN
         })
+        console.log('client', client)
+        client.checkout.create()
+            .then((checkout) => {
+                setCheckout(checkout)
+            })
+    }, [])
+    console.log('checkout:', checkout)
     return (
         <div>
             Hello from Shopping Cart
