d37bf2950cc2038296f8f0c6eab16862820710fa
Andrew St Germain <Andrew.StGermain@BNW.local>
2020-12-07 16:49:06 -0800

    Added gatsby-ssr and added methods to gatsby-browser

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -8,3 +8,17 @@ import "./src/styles/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+
+import React from "react"
+import Layout from "./src/components/layout"
+
+// Logs when the client route changes
+export const onRouteUpdate = ({ location, prevLocation }) => {
+  console.log("new pathname", location.pathname)
+  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
+}
+
+// Wraps every page in a component
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
