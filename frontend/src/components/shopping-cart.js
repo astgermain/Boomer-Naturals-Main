483ecbc524e8e05fd47d31bec22b707cf1c08ca5
Brian Vargas <cbvargas123@gmail.com>
2020-12-15 13:04:45 -0800

    created file for cart items

--- frontend/src/components/shopping-cart.js
+++ frontend/src/components/shopping-cart.js
@@ -1,22 +1,43 @@
 import React, { useState, useEffect, useContext } from "react"
+import CartItem from './cart-item'
+//import { Store } from "../util/store"
+/**
+ * addToCart - variantId, quantity, setIsLoading
+ * buyNow - productID, quantity, setIsLoading
+ * checkout
+ * removeFromCart - lineItemId
+ * setValue - value
+ * toggleCart
+ */
+const ShoppingCart = (props) => {
+  console.log('Cart: ', props)
+  
+  const {
+    addToCart,
+    buyNow,
+    checkout,
+    removeFromCart,
+    setValue,
+    toggleCart
+  } = props.context
 
-import { ClientContext } from "./layout"
+  const PRODUCTS_IN_CART = checkout.lineItems
 
-const ShoppingCart = () => {
+  // const [checkout, setCheckout] = useState({})
 
-    const [checkout, setCheckout] = useState({})
-    const client = useContext(ClientContext)
-
+  return (
+    <h1>ShoppingCart</h1>
+  )
+  //const { checkoutContext, toggleCart, removeFromCart } = useContext(Store)
+  /*
     useEffect(() => {
-        client.checkout.create()
-            .then((checkout) => {
-                setCheckout(checkout)
-            })
+      checkoutContext.create().then(createdCheckout => {
+        setCheckout(createdCheckout)
+      })
     }, [])
-    return (
-        <div>
-        </div>
-    )
+    */
+  return <div></div>
+
 }
 
 export default ShoppingCart
