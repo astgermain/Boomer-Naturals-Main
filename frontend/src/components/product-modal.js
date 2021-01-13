8e8d5a7d4b4fe3cd745fcee3a24874da30e89097
Andrew St Germain <38926995+astgermain@users.noreply.github.com>
2021-01-13 09:50:34 -0800

    Update product-modal.js

--- frontend/src/components/product-modal.js
+++ frontend/src/components/product-modal.js
@@ -13,7 +13,6 @@ import "../styles/upsell.css"
 
 const ProductModal = ({ data, setModalShow }) => {
   const { addToCart } = useContext(store)
-  console.log("HEY!", data)
 
   let x = () => {
     try {
