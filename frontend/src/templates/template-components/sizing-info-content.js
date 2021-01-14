8b13a159d30c735a57b732b99d1d471005fc55bb
Bianca Gonçalves <biancagoncalves@Biancas-MacBook-Pro.local>
2021-01-14 13:16:11 -0800

    added most of the sizing data for products we have on the site, add styling, also changed most of the product types on shopify

--- frontend/src/templates/template-components/sizing-info-content.js
+++ frontend/src/templates/template-components/sizing-info-content.js
@@ -3,65 +3,345 @@ import "../../styles/product-template.css"
 
 const SizingContent = ({ ProductSizes, pageContext }) => {
   const productType = pageContext.node.productType
-
+  //add product to switch statement
   const SwitchingSizingInfo = () => {
     switch (productType) {
+//Apparel
       case "Face Cover":
         return (
+          <div>
+            <table>
+            <tbody>
+              <tr>
+                <th>Sizes</th>
+                <th>W</th>
+                <th>H</th>
+              </tr>
+            </tbody>
+            </table>
           <div className="sizing-image-container">
             <div className="sizes-w-h-flex">
-            <ul >
-            {ProductSizes.Mask.Sizes.map((e) => {return <li key={e}>{e}</li>})}
-            </ul>
-            <ul >
-            {ProductSizes.Mask.W.map((e) => {return <li key={e}>{e}</li>})}
-            </ul>
-            <ul >
-            {ProductSizes.Mask.H.map((e) => {return <li key={e}>{e}</li>})}
-            </ul>
+              <ul>
+                {ProductSizes.Mask.Sizes.map(e => {
+                  return <li key={e}>{e}</li>
+                })}
+              </ul>
+              <ul>
+                {ProductSizes.Mask.W.map(e => {
+                  return <li key={e}>{e}</li>
+                })}
+              </ul>
+              <ul>
+                {ProductSizes.Mask.H.map(e => {
+                  return <li key={e}>{e}</li>
+                })}
+              </ul>
             </div>
 
-            <div className="image-text-grid">
+            <div className="image-text-grid measure-text">
               <img
                 src={ProductSizes.Mask.Image}
                 alt={ProductSizes.Mask.ImageText}
               />
-              <span>{ProductSizes.Mask.ImageText}</span>
+              <span>{ProductSizes.Mask.Name} are measured</span>
+              <span className="light-blue">
+                {ProductSizes.Mask.Wmeasured}
+              </span>
+              <span className="dark-blue">{ProductSizes.Mask.Hmeasured}</span>
             </div>
+          </div>
           </div>
         )
       case "Neck Gaiter":
         return (
-          <div className="sizing-image-container">
-            <div className="sizes-w-h-flex">
-            <ul >
-            {ProductSizes.Gaiter.Sizes.map((e) => {return <li key={e}>{e}</li>})}
-            </ul>
-            <ul >
-            {ProductSizes.Gaiter.W.map((e) => {return <li key={e}>{e}</li>})}
-            </ul>
-            <ul >
-            {ProductSizes.Gaiter.H.map((e) => {return <li key={e}>{e}</li>})}
-            </ul>
-            </div>
+          <div>
+            <table>
+            <tbody>
+              <tr>
+                <th>Sizes</th>
+                <th>W</th>
+                <th>H</th>
+              </tr>
+            </tbody>
+            </table>
+            <div className="sizing-image-container">
+              <div className="sizes-w-h-flex">
+                <ul>
+                  {ProductSizes.Gaiter.Sizes.map(e => {
+                    return <li key={e}>{e}</li>
+                  })}
+                </ul>
+                <ul>
+                  {ProductSizes.Gaiter.W.map(e => {
+                    return <li key={e}>{e}</li>
+                  })}
+                </ul>
+                <ul>
+                  {ProductSizes.Gaiter.H.map(e => {
+                    return <li key={e}>{e}</li>
+                  })}
+                </ul>
+              </div>
 
-            <div className="image-text-grid">
-              <img
-                src={ProductSizes.Mask.Image}
-                alt={ProductSizes.Gaiter.ImageText}
-              />
-              <span>{ProductSizes.Gaiter.ImageText}</span>
+              <div className="image-text-grid measure-text">
+                <img
+                  src={ProductSizes.Gaiter.Image}
+                  alt={ProductSizes.Gaiter.ImageText}
+                />
+                <span>{ProductSizes.Gaiter.Name} are measured</span>
+                <span className="light-blue">
+                  {ProductSizes.Gaiter.Wmeasured}
+                </span>
+                <span className="dark-blue">
+                  {ProductSizes.Gaiter.Hmeasured}
+                </span>
+              </div>
             </div>
           </div>
         )
       case "Shield Cover":
-        return <div className="sizing-image-container"></div>
+        return (
+          <div>
+            <table>
+            <tbody>
+              <tr>
+                <th>Sizes</th>
+                <th>W</th>
+                <th>H</th>
+              </tr>
+            </tbody>
+            </table>
+          <div className="sizing-image-container">
+            <div className="sizes-w-h-flex">
+              <ul>
+                {ProductSizes.Shield.Sizes.map(e => {
+                  return <li key={e}>{e}</li>
+                })}
+              </ul>
+              <ul>
+                {ProductSizes.Shield.W.map(e => {
+                  return <li key={e}>{e}</li>
+                })}
+              </ul>
+              <ul>
+                {ProductSizes.Shield.H.map(e => {
+                  return <li key={e}>{e}</li>
+                })}
+              </ul>
+            </div>
+
+            <div className="image-text-grid measure-text">
+              <img
+                src={ProductSizes.Shield.Image}
+                alt={ProductSizes.Shield.ImageText}
+              />
+              <span>{ProductSizes.Shield.Name} are measured</span>
+              <span className="light-blue">
+                {ProductSizes.Shield.Wmeasured}
+              </span>
+              <span className="dark-blue">{ProductSizes.Shield.Hmeasured}</span>
+            </div>
+          </div>
+          </div>
+        )
+      case "Socks":
+        return <div>sock size </div>
+//Beauty
+      case "Boom Boom Libido Supplement":
+        return (
+          <div>
+            <span>Sizes</span>
+          <div className="sizing-image-container">
+            <div className="sizes-w-h-flex">
+              <ul>
+                {ProductSizes.BoomBoom.Sizes.map(e => {
+                  return <li key={e}>{e}</li>
+                })}
+              </ul>
+            </div>
+          </div>
+          </div>
+        )
+        case "Boom Lashes":
+          return (
+            <div>
+              <span>Sizes</span>
+            <div className="sizing-image-container">
+              <div className="sizes-w-h-flex">
+                <ul>
+                  {ProductSizes.BoomLashes.Sizes.map(e => {
+                    return <li key={e}>{e}</li>
+                  })}
+                </ul>
+              </div>
+            </div>
+            </div>
+          )
+          case "Beauty Creams":
+            return (
+              <div>
+                <span>Sizes</span>
+              <div className="sizing-image-container">
+                <div className="sizes-w-h-flex">
+                  <ul>
+                    {ProductSizes.BeautyCreams.Sizes.map(e => {
+                      return <li key={e}>{e}</li>
+                    })}
+                  </ul>
+                </div>
+              </div>
+              </div>
+            )
+            case "Beauty Facial Mask":
+              return (
+                <div>
+                  <span>Sizes</span>
+                <div className="sizing-image-container">
+                  <div className="sizes-w-h-flex">
+                    <ul>
+                      {ProductSizes.BeautyFacialMask.Sizes.map(e => {
+                        return <li key={e}>{e}</li>
+                      })}
+                    </ul>
+                  </div>
+                </div>
+                </div>
+              )
+              case "Beauty Eye Mask":
+                return (
+                  <div>
+                    <span>Sizes</span>
+                  <div className="sizing-image-container">
+                    <div className="sizes-w-h-flex">
+                      <ul>
+                        {ProductSizes.BeautyEyeMask.Sizes.map(e => {
+                          return <li key={e}>{e}</li>
+                        })}
+                      </ul>
+                    </div>
+                  </div>
+                  </div>
+                )
+                case "Beauty Facial Pads":
+                  return (
+                    <div>
+                      <span>Sizes</span>
+                    <div className="sizing-image-container">
+                      <div className="sizes-w-h-flex">
+                        <ul>
+                          {ProductSizes.BeautyFacialPads.Sizes.map(e => {
+                            return <li key={e}>{e}</li>
+                          })}
+                        </ul>
+                      </div>
+                    </div>
+                    </div>
+                  )
+                case "Beauty Cleanser":
+                  return (
+                    <div>
+                      <span>Sizes</span>
+                    <div className="sizing-image-container">
+                      <div className="sizes-w-h-flex">
+                        <ul>
+                          {ProductSizes.BeautyCleanser.Sizes.map(e => {
+                            return <li key={e}>{e}</li>
+                          })}
+                        </ul>
+                      </div>
+                    </div>
+                    </div>
+                  )
+                  case "Sunscreen":
+                    return (
+                      <div>
+                        <span>Sizes</span>
+                      <div className="sizing-image-container">
+                        <div className="sizes-w-h-flex">
+                          <ul>
+                            {ProductSizes.SunScreen.Sizes.map(e => {
+                              return <li key={e}>{e}</li>
+                            })}
+                          </ul>
+                        </div>
+                      </div>
+                      </div>
+                    )
+                    case "Lip Balm":
+                      return (
+                        <div>
+                          <span>Sizes</span>
+                        <div className="sizing-image-container">
+                          <div className="sizes-w-h-flex">
+                            <ul>
+                              {ProductSizes.LipBalm.Sizes.map(e => {
+                                return <li key={e}>{e}</li>
+                              })}
+                            </ul>
+                          </div>
+                        </div>
+                        </div>
+                      )
+                      case "Boomer Botanics":
+                        return (
+                          <div>
+                            <span>Sizes</span>
+                          <div className="sizing-image-container">
+                            <div className="sizes-w-h-flex">
+                              <ul>
+                                {ProductSizes.BoomerBotanics.Sizes.map(e => {
+                                  return <li key={e}>{e}</li>
+                                })}
+                              </ul>
+                            </div>
+                          </div>
+                          </div>
+                        )
+                        case "Boomer Botanics Gummies":
+                          return (
+                            <div>
+                              <span>Sizes</span>
+                            <div className="sizing-image-container">
+                              <div className="sizes-w-h-flex">
+                                <ul>
+                                  {ProductSizes.BoomerBotanicsGummies.Sizes.map(e => {
+                                    return <li key={e}>{e}</li>
+                                  })}
+                                </ul>
+                              </div>
+                            </div>
+                            </div>
+                          )
+//Food
+      case "Socks":
+        return <div>sock size </div>
+//Home
       case "Pillow":
         return <div>Pillow size</div>
       case "Sheets":
         return <div>Sheet size</div>
-      case "Socks":
-        return <div>sock size </div>
+//PPE
+      case "Hand Sanitizer":
+        return(
+          <div>
+            <table>
+            <tbody>
+              <tr>
+                <th>Sizes</th>
+              </tr>
+            </tbody>
+            </table>
+          <div className="sizing-image-container">
+            <div className="sizes-w-h-flex">
+              <ul>
+                {ProductSizes.HandSanitizer.Sizes.map(e => {
+                  return <li key={e}>{e}</li>
+                })}
+              </ul>
+            </div>
+          </div>
+          </div>
+        )
       default:
         return <div>No Sizing Available</div>
     }
@@ -69,16 +349,7 @@ const SizingContent = ({ ProductSizes, pageContext }) => {
 
   return (
     <div className="sizecontainer">
-      <table>
-  <tr>
-    <th>Sizes</th>
-    <th>W</th>
-    <th>H</th>
-  </tr>
-  </table>
       <SwitchingSizingInfo />
-
-
     </div>
   )
 }
--- frontend/src/templates/template-components/sizing-info.js
+++ frontend/src/templates/template-components/sizing-info.js
@@ -7,22 +7,61 @@ import SizingContent from "./sizing-info-content"
 
 
 const SizingInfo = ({ pageContext, SizingDisable }) => {
-
+//add product data, sizes, images, etc to ProductSizes
+//after add product to switch statement in sizing-info-content
   const ProductSizes = {
     Mask:{
-            Sizes:["Small", "Medium", "Large", "L/XL", "XL" ],
-            W: ["9.5\"", "10.0\"", "10.3\"", "10.3\"", "10.6\""],
-            H: ["5.3\"", "5.8\"", "6.3\"", "6.5\"", "6.8\""],
-            Image: facecoverimage,
-            ImageText:"Face Cover is measured left to right (w), top to bottom (h)"
+            Name:"Face Covers",
+            Sizes:["Ages 2-7", "Ages 8-12", "Small", "Medium", "Large", "L/XL", "XL" ],
+            W: ["8.3\"", "9.2\"","9.5\"", "10.0\"", "10.3\"", "10.3\"", "10.6\""],
+            H: ["4.5\"", "5\"","5.3\"", "5.8\"", "6.3\"", "6.5\"", "6.8\""],
+            Wmeasured: "left to right (w)",
+            Hmeasured: "top to bottom (h)",
+            Image: facecoverimage
     },
     Gaiter:{
-            Sizes:["Small","Large", "XL" ],
-            W: ["9.5\"", "10.3\"", "10.6\""],
-            H: ["5.3\"", "6.5\"", "6.8\""],
-            Image: gaiterimage,
-            ImageText:"Gaiter is measured left to right (w), top to bottom (h)"
-}
+            Name:"Gaiters",
+            Sizes:["Ages 2-7", "Ages 8-12", "S/M","L/XL" ],
+            W: ["7\"", "8\"", "9\"", "10.2\""],
+            H: ["10.5\"", "11.5\"", "14\"", "15\""],
+            Wmeasured: "left to right (w)",
+            Hmeasured: "top to bottom (h)",
+            Image: gaiterimage
+},  Shield:{
+            Name:"Shield Covers",
+            Sizes:["Ages 2-7", "Ages 8-12", "Small", "Medium", "Large", "L/XL", "XL" ],
+            W: ["8.3\"", "9.2\"","9.5\"", "10.0\"", "10.3\"", "10.3\"", "10.6\""],
+            H: ["4.5\"", "5\"","5.3\"", "5.8\"", "6.3\"", "6.5\"", "6.8\""],
+            Wmeasured: "left to right (w)",
+            Hmeasured: "top to bottom (h)",
+            Image: facecoverimage
+},  HandSanitizer:{
+            Sizes:["2oz", "4oz", "10oz"],
+},  BoomBoom:{
+            Sizes:["1 ct", "10 ct"],
+},  BoomLashes:{
+            Sizes:["0.5oz"],
+},  BeautyCreams:{
+            Sizes:["30mL"],
+},  BeautyCleanser:{
+            Sizes:["8oz"],
+},  BeautyFacialPads:{
+            Sizes:["60 ct pads"],
+},  BeautyFacialMask:{
+            Sizes:["1 Mask"],
+},  BeautyEyeMask:{
+            Sizes:["30 ct"],
+},  SunScreen:{
+            Sizes:["2.4 oz", "1.4 oz"],
+},  LipBalm:{
+            Sizes:["50 mg"],
+},  BoomerBotanics:{
+            Sizes:["1 oz"],
+},  BoomerBotanicsGummies:{
+            Sizes:["5 ct", "30 ct", "60 ct"],
+},
+
+
 }
 
   return (
