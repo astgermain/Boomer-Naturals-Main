a9f11b256c1fcaaf52d4162c8e3612b5950898f6
Andrew St Germain <Andrew.StGermain@BNW.local>
2021-01-14 12:19:50 -0800

    Shopping Cart update if product variant becomes unavailable

--- frontend/src/components/shopping-cart.js
+++ frontend/src/components/shopping-cart.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react"
-import store from '../util/store'
-import CartItem from './cart-item'
+import store from "../util/store"
+import CartItem from "./cart-item"
 import { Slide } from "react-awesome-reveal"
 import "../styles/shopping-cart.css"
 
@@ -20,81 +20,84 @@ const ShoppingCart = () => {
     checkout,
     removeFromCart,
     setValue,
-    toggleCart
+    toggleCart,
   } = useContext(store)
   useEffect(() => {
-    console.log('do something')
+    console.log("do something")
   }, [isCartOpen])
 
   const PRODUCTS_IN_CART = checkout.lineItems
   const SUBTOTAL_PRICE = checkout.subtotalPrice
-  const TOTAL_DIFFERENCE_UNTIL_FREE_SHIPPING = (50 - parseFloat(SUBTOTAL_PRICE)).toFixed(2)
+  const TOTAL_DIFFERENCE_UNTIL_FREE_SHIPPING = (
+    50 - parseFloat(SUBTOTAL_PRICE)
+  ).toFixed(2)
 
-  const differenceToDisplay = (currentDifference) => {
+  const differenceToDisplay = currentDifference => {
     if (currentDifference > 0) {
       return (
-        <p>Add <span className="difference-num">{currentDifference}</span> more for <strong>FREE SHIPPING!</strong></p>
-      ) 
+        <p>
+          Add <span className="difference-num">{currentDifference}</span> more
+          for <strong>FREE SHIPPING!</strong>
+        </p>
+      )
     } else {
       return (
-        <p>Congratulations!<br></br>You've earned <strong>FREE SHIPPING!</strong></p>
+        <p>
+          Congratulations!<br></br>You've earned <strong>FREE SHIPPING!</strong>
+        </p>
       )
     }
   }
-
   const SHOPPING_CART_ITEMS = (
-    <ul>
-      {
-        PRODUCTS_IN_CART.map(({ title, quantity, variant, id }) => (
-          <li key={id}>
-            <CartItem
-              productTitle={title}
-              variantTitle={variant.title}
-              quantity={quantity}
-              price={variant.price}
-              imgSrc={variant.image.src}
-              imgAltText={variant.image.altText}
-              extraData={variant}
-              lineItemId={id}
-              variantId={variant.id}
-              removeFromCart={removeFromCart}
-              addToCart={addToCart}
-              linkToProduct={"/"} //need to update
-            />
-          </li>
-        ))
-      }
-    </ul>
-  )
+      <ul>
+        {PRODUCTS_IN_CART.map((data) => {
+          try {
+            console.log("PCCCCC", data)
+            return(
+            <li key={data.id}>
+              <CartItem
+                productTitle={data.title}
+                variantTitle={data.variant.title}
+                quantity={data.quantity}
+                price={data.variant.price}
+                imgSrc={data.variant.image.src}
+                imgAltText={data.variant.image.altText}
+                extraData={data.variant}
+                lineItemId={data.id}
+                variantId={data.variant.id}
+                removeFromCart={removeFromCart}
+                addToCart={addToCart}
+                linkToProduct={"/"} //need to update
+              />
+            </li>
+            )
+          } catch {
+            removeFromCart()
+          }
+        })}
+      </ul>
+    )
+  
 
-  const NO_CART_ITEMS = (
-    <p>No Products in your cart.</p>
-  )
+
+  const NO_CART_ITEMS = <p>No Products in your cart.</p>
 
   useEffect(() => {
-    //console.log('PRODS IN CART', PRODUCTS_IN_CART)
-    //console.log('Checkout: ', checkout)
-
+    //console.log("PRODS IN CART", PRODUCTS_IN_CART)
+    //Fconsole.log("Checkout: ", checkout)
   }, [PRODUCTS_IN_CART])
   return (
-    <Slide
-      triggerOnce={true}
-      direction="right"
-    >
+    <Slide triggerOnce={true} direction="right">
       <section className="shopping-cart-wrapper">
         <header>
           <h3>My Cart</h3>
           <button className="close" onClick={toggleCart}></button>
         </header>
         <div className="shopping-cart-body">
-          <div>Already have an account? <a href="/">Login</a></div>
-          {
-            PRODUCTS_IN_CART.length
-              ?
-              SHOPPING_CART_ITEMS
-              :
-              NO_CART_ITEMS
-          }
+          <div>
+            Already have an account? <a href="/">Login</a>
+          </div>
+          {PRODUCTS_IN_CART.length ? SHOPPING_CART_ITEMS : NO_CART_ITEMS}
         </div>
         <footer>
           <div className="totals-display">
@@ -115,14 +118,17 @@ const ShoppingCart = () => {
             </table>
           </div>
           <div className="cart-redirect-btns">
-            <a className="view-cart" href={'/'}>VIEW MY CART</a>
-            <a className="checkout" href={checkout.webUrl}>CHECK OUT</a>
+            <a className="view-cart" href={"/"}>
+              VIEW MY CART
+            </a>
+            <a className="checkout" href={checkout.webUrl}>
+              CHECK OUT
+            </a>
           </div>
         </footer>
       </section>
     </Slide>
   )
-
 }
 
 export default ShoppingCart
