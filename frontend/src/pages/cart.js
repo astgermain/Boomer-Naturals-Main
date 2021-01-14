01362d25e29f066c076b58391100a5c26511abde
cbvargas5 <cbvargas123@gmail.com>
2021-01-14 14:48:37 -0800

    set up structure for table

--- frontend/src/pages/cart.js
+++ frontend/src/pages/cart.js
@@ -4,7 +4,40 @@ import Layout from "../components/layout"
 const ShoppingCartPage = ({ location }) => {
     return (
         <Layout location={location}>
-            <h3>ShoppingCart</h3>
+            <section className="shopping-cart-page">
+                <header>
+                    <h2>My Cart</h2>
+                </header>
+                <div>
+                    {/* Iterate through product map */}
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th>PRODUCT</th>
+                                <th>PRICE</th>
+                                <th>QUANTITY</th>
+                                <th>TOTAL</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>x</td>
+                                <td>img</td>
+                                <td>product info</td>
+                                <td>$9.99</td>
+                                <td>qty: 1</td>
+                                <td>$9.99</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <footer>
+                    <div></div>
+                    <div></div>
+                </footer>
+            </section>
         </Layout>
     )
 }
