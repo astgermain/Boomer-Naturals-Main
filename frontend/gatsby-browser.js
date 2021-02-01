6e7358ba35454930c5fd9c6475fc2092de979d81
cbvargas5 <cbvargas123@gmail.com>
2021-02-01 10:54:33 -0800

    changes imports for gatsby-browser file

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -2,9 +2,7 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
-import "./src/styles/normalize.css"
-// custom CSS styles
-import "./src/styles/style.css"
+import { NORMALIZE_CSS, STYLE_CSS } from "./src/util/imports"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
