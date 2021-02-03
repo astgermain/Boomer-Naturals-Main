57e8e6c6ffc278332f66381b057229b3128210e3
Bianca Gonçalves <biancagoncalves@Biancas-MacBook-Pro.local>
2021-02-03 15:59:11 -0800

    created an info bar and mobile banner

--- frontend/src/components/home-layout.js
+++ frontend/src/components/home-layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Hero from "./home-items/hero"
+import InfoBanner from "./home-items/info-banner.js"
 import Categories from "./home-items/categories"
 import AsSeenOn from "./home-items/as-seen-on"
 import Email from "./home-items/email"
@@ -8,12 +9,15 @@ import Featured from "./home-items/featured"
 import AboutFaceMask from "./home-items/about-face-mask"
 import Insta from "./home-items/insta"
 import News from "./home-items/news"
+import HeroMobile from "./home-items/hero-mobile"
 
 
 const HomeLayout = ({data}) => {
   return (
     <main className="main-section">
-      <Hero />
+      <InfoBanner/>
+      <Hero/>
+      <HeroMobile/>
       <Featured data={data}/>
       <Categories />
       <ProductCarousel whichPage={"product-carousel"} />
